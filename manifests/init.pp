e4284f570675b8c9965b542ff5590bc98490423b
Keith Burdis <keith@burdis.org>
2012-10-25 18:39:12 +0100

    Use a fixed basedir when installing and configuring.  puppet-lint compliance changes.  Parameterised classes rather than using class variables.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,11 @@
-class anthillpro_agent {
-  $anthill_remote_host = '10.5.1.109'
-  $install_tarball = 'Anthill Agent Installation 3.7.3.tar.gz'
-  $basedir = '/var/lib/puppet/workspace/anthillpro_agent'
-  include anthillpro_agent::config,
-    anthillpro_agent::install,
-    anthillpro_agent::service
+class anthillpro_agent (
+  $remote_host = '10.5.1.109',
+  $tarball     = 'Anthill Agent Installation 3.7.3.tar.gz'
+) {
+  class { 'anthillpro_agent::config':
+    remote_host => $remote_host,
+    tarball     => $tarball,
+  }
+  include anthillpro_agent::install
+  include anthillpro_agent::service
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,15 +1,17 @@
 class anthillpro_agent::install {
-  realize( Package["jdk", "perl", "tar", "gzip"] )
-  exec { "install":
-    command     => "$anthillpro_agent::basedir/anthill3-install/unattended-install-agent.sh",
-    require     => Class["anthillpro_agent::config"],
-    creates     => "/opt/anthill/agents/deployer-0",
+  package { ['jdk', 'perl', 'tar', 'gzip']:
+    ensure => installed,
   }
-  file { "/opt/anthill/agents/deployer-0/installed.properties":
+  exec { 'install':
+    command     => '/root/anthillpro_agent/anthill3-install/unattended-install-agent.sh',
+    require     => Class['anthillpro_agent::config'],
+    creates     => '/opt/anthill/agents/deployer-0',
+  }
+  file { '/opt/anthill/agents/deployer-0/installed.properties':
     ensure  => present,
-    mode    => 444,
-    content => template("anthillpro_agent/installed.properties.erb"),
-    notify  => Class["anthillpro_agent::service"],
-    require => Exec["install"],
+    mode    => '0444',
+    content => template('anthillpro_agent/installed.properties.erb'),
+    notify  => Class['anthillpro_agent::service'],
+    require => Exec['install'],
   }
 }

19d6866662b854a5b72523c71ac156976f02443f
Keith Burdis <keith@burdis.org>
2012-10-29 14:01:43 +0000

    Use hiera by default. Avoid creating temporary variables.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,26 @@
 class anthillpro_agent (
   $remote_host = '10.5.1.109',
   $tarball     = 'Anthill Agent Installation 3.7.3.tar.gz',
-  $use_hiera   = false
+  $use_hiera   = true
 ) {
   if $use_hiera {
-    $anthillpro_agent = hiera['anthillpro_agent']
-    $_remote_host = $anthillpro_agent['remote_host'] ? {
-      undef   => $remote_host,
-      default => $anthillpro_agent['remote_host']
-    }
-    $_remote_host = $anthillpro_agent['tarball'] ? {
-      undef   => $remote_host,
-      default => $anthillpro_agent['tarball']
+    $anthillpro_agent = hiera('anthillpro_agent')
+    class { 'anthillpro_agent::config':
+      remote_host => $anthillpro_agent['remote_host'] ? {
+        undef   => $remote_host,
+        default => $anthillpro_agent['remote_host']
+      },
+      remote_host => $anthillpro_agent['tarball'] ? {
+        undef   => $remote_host,
+        default => $anthillpro_agent['tarball']
+      },
     }
   }
   else {
-    $_remote_host = $remote_host
-    $_tarball     = $tarball
-  }
-  class { 'anthillpro_agent::config':
-    remote_host => $_remote_host,
-    tarball     => $_tarball,
+    class { 'anthillpro_agent::config':
+      remote_host => $remote_host,
+      tarball     => $tarball,
+    }
   }
   include anthillpro_agent::install
   include anthillpro_agent::service

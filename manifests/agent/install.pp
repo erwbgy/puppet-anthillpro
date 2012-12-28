9d32abd05209e680688c927a469c3e6220fa3f56
Keith Burdis <keith@burdis.org>
2012-12-28 12:06:42 +0000

    Merged in config.pp and made things configurable.

--- manifests/agent/install.pp
+++ manifests/agent/install.pp
@@ -1,13 +1,53 @@
-class anthillpro::agent::install {
+class anthillpro::agent::install (
+  $agent_root,
+  $applications,
+  $cache_root,
+  $deploy_root,
+  $java_home,
+  $log_root,
+  $remote_host,
+  $remote_port,
+  $tarball,
+) {
+  # defaults
+  File {
+    owner => 'root',
+    group => 'root',
+  }
+  file { '/root/anthillpro::agent':
+    ensure => directory,
+  }
+  file { 'anthillpro::agent-tarball':
+    ensure  => present,
+    path    => "/root/anthillpro::agent/${tarball}",
+    mode    => '0444',
+    source  => "puppet:///files/${tarball}",
+    require => File['/root/anthillpro::agent'],
+  }
+  exec { 'anthillpro::agent-unpack':
+    command     => "/bin/tar -C /root/anthillpro::agent -zxf '/root/anthillpro::agent/${tarball}'",
+    require     => File['anthillpro::agent-tarball'],
+    creates     => '/root/anthillpro::agent/anthill3-install',
+  }
+  file { '/root/anthillpro::agent/anthill3-install/unattended-install-agent.sh':
+    ensure  => present,
+    mode    => '0555',
+    content => template('anthillpro/agent/unattended-install-agent.sh.erb'),
+    require => Exec['anthillpro::agent-unpack'],
+  }
+  file { "${log_root}/anthillpro::agent.log":
+    ensure => link,
+    target => "${agent_root}/agents/deployer-0/var/log/ah3agent.out",
+  }
   if ! defined(Package['perl']) { package { 'perl': ensure => installed, } }
   if ! defined(Package['tar'])  { package { 'tar':  ensure => installed, } }
   if ! defined(Package['gzip']) { package { 'gzip': ensure => installed, } }
   exec { 'anthill-install':
     command     => '/root/anthillpro::agent/anthill3-install/unattended-install-agent.sh',
-    require     => Class['anthillpro::agent::config'],
-    creates     => '/opt/anthill/agents/deployer-0',
+    require     => File['agent/anthill3-install/unattended-install-agent.sh'],
+    creates     => "${agent_root}/agents/deployer-0",
   }
-  file { '/opt/anthill/agents/deployer-0/installed.properties':
+  file { "${agent_root}/agents/deployer-0/installed.properties":
     ensure  => present,
     mode    => '0444',
     content => template('anthillpro/agent/installed.properties.erb'),
